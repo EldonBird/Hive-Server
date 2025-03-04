ad52a2fa82f6b0bf1f728cc1de983119478d8e29
Eldon <Birdheldon@gmail.com>
2025-03-04 01:39:07 -0700

    basic functionality

---
+++ TestingServer.py
@@ -0,0 +1,24 @@
+﻿from flask import Flask, request
+from datetime import datetime
+
+app = Flask(__name__)
+
+def output_callback(data):
+    for i in range(len(data)):
+        print(i)
+
+@app.post('/submission')
+def submission():
+
+    temp = float(request.form.get('temp'))
+    hum = float(request.form.get('hum'))
+
+    output_callback(temp + hum)
+    return str("complete")
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=52067)
